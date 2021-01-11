33af761cd204d8974b5e96ed8fb733d8ac1b6a20
Harshita Atray <harshitaatray@gmail.com>
2021-01-11 20:32:44 +0530

    final changes

--- dustbin.js
+++ dustbin.js
@@ -1,66 +1,36 @@
-class dustbin
+class Dustbin
 {
 	constructor(x,y)
 	{
 		this.x=x;
 		this.y=y;
-		this.dustbinWidth=200;
-		this.dustbinHeight=100;
-		this.wallThickness=20;
-		this.angle=0;	
+		this.width=200;
+		this.height=100;
 		
-		this.bottomBody=Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, {isStatic:true})
-		this.leftWallBody=Bodies.rectangle(this.x-this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
-		Matter.Body.setAngle(this.leftWallBody, this.angle);
-		
-
-		this.rightWallBody=Bodies.rectangle(this.x+this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
-		Matter.Body.setAngle(this.rightWallBody, -1*this.angle);
-		World.add(world, this.bottomBody)
-		World.add(world, this.leftWallBody)
-		World.add(world, this.rightWallBody);
+		this.body1=Bodies.rectangle(this.x, this.y, this.width, 4, {isStatic:true});
+		this.body2=Bodies.rectangle(this.x-100, this.y-50, 4, this.height, {isStatic:true});
+		this.body3=Bodies.rectangle(this.x+100, this.y-50, 4, this.height, {isStatic:true});
+		World.add(world, this.body1);
+		World.add(world, this.body2);
+		World.add(world, this.body3);
 
 	}
 	display()
 	{
-			var posBottom=this.bottomBody.position;
-			var posLeft=this.leftWallBody.position;
-			var posRight=this.rightWallBody.position;
-
+			var pos=this.body1.position;
 			
-
 			push()
-			translate(posLeft.x, posLeft.y);
+			translate(pos.x, pos.y);
 			rectMode(CENTER)
-			//strokeWeight(4);
-			angleMode(RADIANS)
-			fill(255)
-			stroke(255)
-			rotate(this.angle)
-			rect(0,0,this.wallThickness, this.dustbinHeight);
-			pop()
-
-			push()
-			translate(posRight.x, posRight.y);
-			rectMode(CENTER)
-			//strokeWeight(4);
-			stroke(255)
-			angleMode(RADIANS)
-			fill(255)
-			rotate(-1*this.angle)
-			rect(0,0,this.wallThickness, this.dustbinHeight);
-			pop()
-
-			push()
-			translate(posBottom.x, posBottom.y);
-			rectMode(CENTER)
-			//strokeWeight(4);
-			stroke(255)
-			angleMode(RADIANS)
-			fill(255)
-			rect(0,0,this.dustbinWidth, this.wallThickness);
-			pop()
+			strokeWeight(4);
 			
+			stroke(255)
+			
+			rect(0,0,this.width, 4);
+			rect(-100,-50,4,this.height);
+			rect(100,-50,4,this.height);
+			pop()
+
 	}
 
 }
